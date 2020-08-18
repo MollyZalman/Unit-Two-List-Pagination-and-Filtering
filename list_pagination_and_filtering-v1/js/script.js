10c0a554e2784a821b5871ab18eaa8f3e597d5b0
MollyZalman <68440011+MollyZalman@users.noreply.github.com>
2020-08-18 12:32:41 -0400

    First Additions:
    Setting up student, page and Pagination contents. Trying a few different methods, very rough draft.

--- list_pagination_and_filtering-v1/js/script.js
+++ list_pagination_and_filtering-v1/js/script.js
@@ -6,45 +6,61 @@ FSJS project 2 - List Filter and Pagination
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
+//Get all students
+const studentList = document.querySelector('.student-item cf');
 
+//Get only student names
+const studentName = document.getElementsByTagName('h3');
 
+//Show only 10 students per page
+const itemsPerPage = 10;
 
+//The current page you're on 
+const presentPage = 1;
 
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
+//Shows correct page of students
+//Inspired by https://codepen.io/karpovsystems/pen/fFHxK
+function showPage(page, list) {
+   const listLength = list.length;
 
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
+   //to loop over each item and hide items not requested
+   for (let i = 0; i < listLength; i++) {
+      list[i].style.display = '';
+   } else {
+      list[i].style.display = 'none';
+   }
 
+   const startIndex = page * rowsPerPage;
+   const endIndex = startIndex + rowsPerPage;
 
+   for (let i = startIndex; i < endIndex; i++) {
+      if(students[i]) {
+         students[i].style.display = "block";
+      }
+   }
+}
 
+// inspired by Tania Rascia's blog: https://www.taniarascia.com/how-to-paginate-an-array-in-javascript/
+const previous = document.getElementById('previous');
+const forward = document.getElementById('forward');
 
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
+//allows you to go back a page
+function prev (present, pages) {
+   let index = pages.indexOf(present);
+   if (index === 0) {
+      return pages[pages.length - 1]
+   }
+   return pages [index - 1]
+}
 
-
-
+//allows you to go forward a page
+function next (present, pages) {
+   let index = pages.indexOf(present);
+   if (index === pages.length - 1) {
+      return pages [0];
+   }
+   return pages[index + 1]
+}
 
 
 // Remember to delete the comments that came with this file, and replace them with your own code comments.
