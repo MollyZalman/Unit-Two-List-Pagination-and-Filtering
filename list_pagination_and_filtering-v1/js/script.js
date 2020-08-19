0d525f6ce616b1efc99a4b7f75097c7659549a18
MollyZalman <68440011+MollyZalman@users.noreply.github.com>
2020-08-19 12:54:24 -0400

    Search Feature, clean up
    Added the begining of a search filter, and trying to make sense of things

--- list_pagination_and_filtering-v1/js/script.js
+++ list_pagination_and_filtering-v1/js/script.js
@@ -31,7 +31,20 @@ function showPage(page, list) {
             list[i].style.display = 'none';
          }
       }
-   
+
+//Searchbar function and adding functionality
+function studentSearch = (list) => {
+   const divPageHeader = document.querySelector('div.page-header');
+   const div = document.createElement('div');
+   //Inspired by https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set/add#:~:text=The%20add()%20method%20appends,end%20of%20a%20Set%20object.
+   div.classList.add('student-search');
+   pageDivHeader.appendChild(div);
+   const input = document.createElement('input');
+   const button = document.createElement("BUTTON");
+   button.innerHTML = "Let's go!";
+   div.append(input, button);
+}
+
 // inspired by Tania Rascia's blog: https://www.taniarascia.com/how-to-paginate-an-array-in-javascript/
 //allows you to go back a page
 function prev (present, pages) {
@@ -39,17 +52,23 @@ function prev (present, pages) {
    if (index === 0) {
       return pages[pages.length - 1]
    }
-   return pages [index - 1]
+   return pages [index - 1] 
 }
+   document.getElementById('prev').addEventListener('click', () => {
+      pages.back();
+   });  
 
 //allows you to go forward a page
-function next (present, pages) {
-   let index = pages.indexOf(present);
+function next (presentPage, pages) {
+   let index = pages.indexOf(presentPage);
    if (index === pages.length - 1) {
       return pages [0];
    }
    return pages[index + 1]
 }
+document.getElementById('next').addEventListener('click', () => {
+   pages.forward();
+});  
 
    const startIndex = page * rowsPerPage;
    const endIndex = startIndex + rowsPerPage;
@@ -62,23 +81,23 @@ function next (present, pages) {
 }
 
 //appends and creates functionality for the pagination links at the bottom of the page
-function appendPageLinks(list) {
+function appendPageLinks = (list) => {
    const divDetails = document.querySelector('.page');
    const newDiv = document.createElement('div');
    newDiv.className = 'pagination';
-   divDetails.appendChild(div);
+   divDetails.appendChild(pagination);
    const newUl = document.createElement('ul');
-   newDiv.appendChild(newUl);
+   pagination.appendChild(newUl);
    const amountOfPages = Math.ceil(listLength / itemsPerPage);
-   
+
    //New li button
    //Inspired by https://www.w3schools.com/jsref/met_element_setattribute.asp
    function newLiLink(pageNumber)
-   //SOMETHING IS WRONG HERE IN TERMS OF CONST
-   const link = document.createElement('a');
+   //SOMETHING IS WRONG HERE ON LINE 98
+   newLiLink = document.createElement('a');
    link.setAttribute('href', '#');
    link.textContent = pageNumber;
-      if(pageNumber === 1){
+      if(pageNumber === 1) {
          link.className = 'active';
    }
    ul.appendChild(li);
